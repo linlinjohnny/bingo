f739c34b6092242b09640640d5113da0cfc0cec4
yhlin <yhlin@mail.com>
2015-11-24 23:11:31 +0800

    createTest

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,15 +36,6 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
 
 // production error handler
 // no stacktraces leaked to user
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var Q = require('q');
+var userLib = require('../lib/userLib');
+userLib.get({
+    userID:1,
+    is_get_one:true
+}, function(data){
+console.log(data);
+console.log(data[0].id);
+    // var rdata = {};
+    // rdata.id=data[0].id;
+console.log(rdata)
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
+});
+
+router.get('/register', function(req, res, next) {
+    res.render('user_register', { title: '註冊' });
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('user_login', { title: '登入' });
+});
+
+router.get('/name/:account', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.post('/ax_register', function(req, res, next) {
+console.log(req.body);
+
+    userLib.create(req.body,
+    function(data){
+res.send('ddddddddddddd');
+    });
+        res.render('user_login', { title: '登入' });
+
 });
 
 module.exports = router;
